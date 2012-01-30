5e19d738e876261002efaa197b9961df14119b88
Guy Oron <guyo@goingon.com>
2012-01-29 18:43:52 -0800

    Added documentation on jqmulti_get_version().

--- jqmulti.api.php
+++ jqmulti.api.php
@@ -40,3 +40,32 @@
  /**
   * @} End of "addtogroup hooks".
   */
+
+/**
+ * Gets the version of jQuery to load.
+ * 
+ * Modules that rely on jQuery Multi can
+ * use this method in their installation procedures to make sure that a
+ * satisfactory version of jQuery is present.
+ *
+ * @param $reset 
+ *   whether to reset the cached version number. When using this to check for
+ *   a required jQuery version, it's a good idea to set this to TRUE
+ */
+function jqmulti_get_version($reset = FALSE) {
+  $libraries = libraries_get_libraries();
+  if (isset($libraries['jquery'])) {
+    $files = jqmulti_get_library_files('jquery', $reset);
+    if ($files) {
+      foreach ($files as $file) {
+        // Get the file name.
+       $version = jqmulti_jquery_version_from_path($file);
+       if ($version) {
+         cache_set('jqmulti_version', $version);
+         return $version;
+       }
+      }
+    }
+  }
+  return FALSE;
+}
